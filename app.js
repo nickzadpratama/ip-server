c5c85217d38b1a891e2399e063375a3a0350bfff
“nickzad” <pratamanickzad7@gmail.com>
2026-08-28 17:31:48 +0700

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const router = require("./routers");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
