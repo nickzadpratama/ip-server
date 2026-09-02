665013f881fdaff79800e2d8823e3b90be41ff4f
“nickzad” <pratamanickzad7@gmail.com>
2026-09-02 17:23:43 +0700

    done

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 const express = require("express");
+var cors = require("cors");
 const router = require("./routers");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.set('query parser', 'extended'); 
+app.use(cors());
+app.set("query parser", "extended");
 
 app.use("/", router);
 
