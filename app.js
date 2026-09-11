d4d19fa37b391171f5ac351c8a61ea5b711e5c0f
“nickzad” <pratamanickzad7@gmail.com>
2026-09-11 11:19:33 +0700

    s commit

--- app.js
+++ app.js
@@ -6,6 +6,28 @@ var cors = require("cors");
 const router = require("./routers");
 const errorHandler = require("./middlewares/errorHandler");
 const app = express();
+const { GoogleGenAI } = require("@google/genai");
+
+app.get("/genAI", async (req, res, next) => {
+  try {
+    const { inputAI } = req.body;
+    const id = new GoogleGenAI({
+      apikey: "AQ.Ab8RN6KxQ8TMcABTnQ9mgKJZmc4ReIixJvisazzC5mIuVZkQww",
+    });
+
+    const interaction = await axios.interaction.create({
+      model: "gemini-3.7-flash",
+      input: `${inputAI}, jawab hanya seputar sepak bola Indonesia`,
+    });
+
+    const result = interaction.output_text;
+
+    res.status(200).json({ result });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
